6fcdf0af45d76ba98577d8e97ce9613e04be7073
Nishaad27 <121814341+Nishaad27@users.noreply.github.com>
2025-01-24 13:32:14 +0530

    Update test_operations.py

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -5,7 +5,7 @@ def test_add():
     assert add(-1,1) == 0
     
 def test_sub():
-    assert add(5,2) == 3
-    assert add(4,3) == 1
-    assert add(2,2) == 0
-    assert add(1,2) == -1
+    assert sub(5,2) == 3
+    assert sub(4,3) == 1
+    assert sub(2,2) == 0
+    assert sub(1,2) == -1
